a9c9cbbaeac3669464fd143bf2cee67e62d3bea4
davidnx01 <info@webdavid.sk>
2024-10-09 18:55:17 +0200

    edit: show images

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,9 @@ const nextConfig = {
       "localhost", "127.0.0.1",
       'api.hookah.sk',
       'authentic-desk-7c3fa038e2.strapiapp.com',
-      'authentic-desk-7c3fa038e2.media.strapiapp.com'
+      'authentic-desk-7c3fa038e2.media.strapiapp.com',
+      'complete-unity-a8415c6467.media.strapiapp.com',
+      'complete-unity-a8415c6467.strapiapp.com',
     ]
   }
 };
