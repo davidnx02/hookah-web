4f40a2b1c605da05186857bbc83f21145ec94a1e
davidnx01 <info@webdavid.sk>
2025-04-04 20:33:19 +0200

    edit: maintenance mode enabled

--- src/app/(smoking)/layout.tsx
+++ src/app/(smoking)/layout.tsx
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
     viewport: "width=device-width, initial-scale=1",
 };
 
-const maintananceMode = false;
+const maintananceMode = true;
 
 export default function RootLayout({
   children,
